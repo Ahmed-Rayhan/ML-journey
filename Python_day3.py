b7d000e5d83f74645db9588c70f406810743a73b
Ahmed Rayhan <69686072+Ahmed-Rayhan@users.noreply.github.com>
2025-01-13 23:48:50 +0600

    Create Python_day3.py

---
+++ Python_day3.py
@@ -0,0 +1,45 @@
+#January 13
+
+#task 1
+print('Task 1 ')
+n = 1
+i = 0
+while i < 3:
+    j = 0
+    while j < 3:
+        print(n, end=" ")
+        n += 1
+        j += 1
+    print()
+    i += 1
+
+
+#task 2 (a)
+print('Task 2a ')
+for i in range(4):      
+    for j in range(4):  
+        print("*", end="") 
+    print() 
+
+#task 2 (b)
+print('Task 2b ')
+for i in range(4):        
+    for j in range(i + 1):    
+        print("*", end="")   
+    print()               
+
+#task 2 (c)
+print('Task 2c ')
+for i in range(1, 4 + 1):  
+    for j in range(i):    
+        print(i, end=" ")  
+    print()    
+
+#task 2 (d)
+print('Task 2d ')
+for i in range(4):  
+    num = 1          
+    for j in range(i + 1): 
+        print(num, end=" ") 
+        num += 2       
+    print()     
