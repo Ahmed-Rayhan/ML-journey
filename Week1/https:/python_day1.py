52ef8dbfcf375dce2d4f31b7c01b7ff4b74c04fd
Ahmed Rayhan <69686072+Ahmed-Rayhan@users.noreply.github.com>
2025-01-15 20:58:27 +0600

    Create python_day1.py

---
+++ Week1/https:/python_day1.py
@@ -0,0 +1,22 @@
+#January 11
+
+#task 1
+a="ABRAKADABRA"
+print(a[2])
+
+#task 2
+b="The quick brown fox jumps over the lazy dog"
+print(b[10:19])
+
+#task 3
+A="বাংলা" 
+B="আমার"
+C="সোনার"
+m=B+" "+ C +" "+A
+print(m)
+
+#task 4
+singer_name="Adele"
+song_name="Skyfall"
+message = f"My favorite singer is {singer_name}, whose most popular song is {song_name}."
+print(message)
