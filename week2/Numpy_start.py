15ed19062b4908035290ae96b2a9f59b070a6810
Ahmed Rayhan <69686072+Ahmed-Rayhan@users.noreply.github.com>
2025-01-19 23:25:11 +0600

    Create Numpy_start.py

---
+++ week2/Numpy_start.py
@@ -0,0 +1,14 @@
+#day1
+import numpy as np
+
+print("Task 1")
+x=np.array([[1,2,3,4,5],
+           [6,7,8,9,10]])
+y=np.transpose(x)
+print(y)
+
+print("Task 2")
+a=np.array([1,2,3,4,5])
+b=np.array([6,7,8,9,10])
+c=np.add(a,b)
+print(c)
